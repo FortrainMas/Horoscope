ba81afcd2dcfabd9b921591faf4c6e06dddd4e7b
unknown <vanamokanov8@gmail.com>
2020-07-06 01:10:56 +0700

    Теперь сервер отправляет случайно сгенерированый гороскоп под каждый из знаков зодиака, упоминая этот знак.

--- server/routes/takeHoroscope.js
+++ server/routes/takeHoroscope.js
@@ -1,12 +1,15 @@
 const {Router} = require('express');
 const router = Router();
+const {makePrediction} = require('../logic/generator');
 
 router.get(
     '', 
     async (req, res) => {
+        const {sign} = req.query;
+        const prediction = await makePrediction(sign);
         res.json(
             {
-                "Server": "alive"
+                "Server": prediction
         })
     }
 )
--- server/server.js
+++ server/server.js
@@ -3,5 +3,6 @@ const config = require('config')
 const PORT = config.get('port') || 5000
 
 const app = express()
+
 app.use('/', require('./routes/takeHoroscope'))
 app.listen(PORT, ()=>{ console.log(`I'm alive on port ${PORT}`) })
